3df94a3aad0bf4c8fe2a9737a7269a282d21f99f
KingDenari <abeldena123@gmail.com>
2025-05-04 19:20:11 +0300

    Added main.py and __init__.py

---
+++ website/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+
+db = SQLAlchemy()
+mail = Mail()  # Flask-Mail instance
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'yuytr jhf'  # Replace with your secret key
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite' # or your actual DB URI
+
+    # Email configuration for Gmail
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'abeldena123@gmail.com'         # Replace with your Gmail
+    app.config['MAIL_PASSWORD'] = 'kxto ddfp tpoq tjtv'       # Use a Gmail app password!
+
+    db.init_app(app)
+    mail.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
