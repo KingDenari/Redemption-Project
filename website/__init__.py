12f8a2eeb2d90c658dbcfd45b20eae60ee7eeacf
KingDenari <abeldena123@gmail.com>
2025-05-07 21:45:23 +0300

    __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -15,8 +15,8 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'abeldena123@gmail.com'         # Replace with your Gmail
-    app.config['MAIL_PASSWORD'] = 'kxto ddfp tpoq tjtv'       # Use a Gmail app password!
+    app.config['MAIL_USERNAME'] = 'redemptioncustomercare1@gmail.com'         # Replace with your Gmail
+    app.config['MAIL_PASSWORD'] = 'gufo zvxi fnvn dnyq'       # Use a Gmail app password!
 
     db.init_app(app)
     mail.init_app(app)
@@ -41,4 +41,6 @@ def create_app():
         return User.query.get(int(id))
 
     return app
+
+
 
